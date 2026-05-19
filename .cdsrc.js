aefed8f2455841aaa8aa5187d1e658dc833c55ba
TharaniDJ <tharanij2002@gmail.com>
2026-05-19 13:22:40 +0530

    Update .cdsrc.js

--- .cdsrc.js
+++ .cdsrc.js
@@ -8,7 +8,17 @@ module.exports = {
                 url: process.env.DATABASE_URL,
                 ssl: {
                     rejectUnauthorized: false
-                }
+                },
+                // Kill queries that run longer than 30 s so UI5 gets a proper error
+                // instead of a silent "ResourceRequest timed out"
+                statement_timeout: 30000,
+                connectionTimeoutMillis: 10000
+            },
+            pool: {
+                min: 2,
+                max: 10,
+                acquireTimeoutMillis: 10000,
+                idleTimeoutMillis: 30000
             }
         }
     }
